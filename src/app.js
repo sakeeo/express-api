bc8ed79980875d1ae69a8e76c1a6b7638d4cbc98
sakeeo <ig.muhammmmadlim@gmail.com>
2023-03-15 06:20:27 +0800

    fix error

--- src/app.js
+++ src/app.js
@@ -18,7 +18,7 @@ app.get("/", (req, res) => {
   });
 });
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
 });
--- src/models/m_user.js
+++ src/models/m_user.js
@@ -14,6 +14,5 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.STRING,
     },
   });
-
   return User;
 };
